11e3d3bb79f7a1ee7d43763becd7b5e2bab115d1
Zeal <mrkingtero@yahoo.com>
2018-02-13 14:36:10 -0800

    require env

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -7,6 +7,7 @@ var basename  = path.basename(__filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require('../config/config.json')[env];
 var db        = {};
+const dotenv  = require("dotenv").config();
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable], config);
